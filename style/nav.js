ae7d69835de8a8abd177346baed1741425864722
yeazin <naz.yeasin@gmail.com>
2025-03-26 12:26:33 +0600

    added custom navbar

---
+++ style/nav.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+    
+    document
+    .querySelector('[data-toggle="offcanvas"]')
+    .addEventListener("click", function () {
+    document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+    })();
+    
